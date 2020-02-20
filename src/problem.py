08926c49b8e7637beda1d8b81e600f10f4220437
luispcunha <luispplmcunha@gmail.com>
2020-02-20 19:38:07 +0000

    meh

--- src/problem.py
+++ src/problem.py
@@ -1,3 +1,5 @@
+from library import Library
+
 class Problem:
     def __init__(self, num_b, num_l, num_d, books, libraries):
         self.num_b = num_b
@@ -6,6 +8,11 @@ class Problem:
         self.books = books
         self.libraries = libraries
 
+    def eval(self, library, days_left):
+        useful_days = days_left - library.signup
+        books_count = useful_days * library.ship
+
+        return sum([book.score for book in library.books])
 
     def print(self):
         print(self.num_b)
